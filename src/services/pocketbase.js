22f2bd1ca14237bfd1d033a04d2fc864fa05e7cd
Rob8624 <robert_melen@yahoo.co.uk>
2024-09-27 09:44:20 +0100

    updated line spacing

--- src/services/pocketbase.js
+++ src/services/pocketbase.js
@@ -1,7 +1,6 @@
 import PocketBase from 'pocketbase';
 
-const pb = new PocketBase('https://pocketbase-production-1ec5.up.railway.app');
-
+const pb = new PocketBase('https://pocketbase-production-1ec5.up.railway.app'); 
 export const fetchHeaderData = async () => {
     const data = await pb.collection('Header_Details').getList();
     return data;
