a7b6ca63584ed3730d30ee7b8bb33f64d0d6fafe
Rob8624 <robert_melen@yahoo.co.uk>
2024-09-30 10:02:11 +0100

    added toggling to about section

--- src/components/about.js
+++ src/components/about.js
@@ -1,7 +1,20 @@
 import React from 'react'
 import '../css/about.css';
+import { useState } from 'react';
 
 export default function About({ aboutData, skillsLogos }) {
+    const [isAboutVisible, setIsAboutVisible] = useState(false)
+    const [isSkillsVisible, setIsSkillsVisible] = useState(false)
+
+    const toggleAboutText = () => {
+        setIsAboutVisible(!isAboutVisible)
+        
+    }
+
+    const toggleSkillsText = () => {
+        setIsSkillsVisible(!isSkillsVisible)
+    }
+
   return(
     
     <div className='flex-wrapper'>
@@ -9,19 +22,19 @@ export default function About({ aboutData, skillsLogos }) {
         {aboutData.map((items, index) => (
             <div className='about-wrapper' key={items.id}>
                 <div className='about-content'>
-                    <div className='about-title'>About</div>
+                    <div className='about-title' onClick={toggleAboutText}>About</div>
                     <i className="fa-regular fa-address-card centre-icon"></i>
                 </div>
-                <div dangerouslySetInnerHTML={{ __html: items.About }} className='about-text'></div>
-                <div className='skills-title'>Skills</div>
+                {isAboutVisible && <div dangerouslySetInnerHTML={{ __html: items.About }}  className='about-text'></div>}
+                <div className='skills-title' onClick={toggleSkillsText}>Skills</div>
+               {isSkillsVisible && (
                 <div className='logo-grid'>
                     {skillsLogos.map((items, index) => (
                             <div className='logo-item'>
                                 <img className="logo-image" src={items} alt="skillslogo"></img>
                             </div>
-                        
                     ))}
-                </div>
+                </div> )}
             </div>)
         )}
     </div>
