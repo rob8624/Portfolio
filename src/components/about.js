3d27dfdfa494576bae2d0f005ac12895be606820
Rob8624 <robert_melen@yahoo.co.uk>
2024-10-01 16:14:26 +0100

    fixed about section expanding past body

--- src/components/about.js
+++ src/components/about.js
@@ -25,10 +25,11 @@ const handleSkillsClick = () => {
             <div className='about-wrapper' key={items.id}>
                 <div className='about-content'>
                     <div className='about-title' onClick={handleAboutClick}>About</div>
-                    <i className="fa-regular fa-address-card centre-icon"></i>
+                    <i class="fa-solid fa-chevron-down"></i>
                 </div>
                <div dangerouslySetInnerHTML={{ __html: items.About }} className={`about-text ${activeIndex === 1 ? 'active' : ''}`} ></div>
                 <div className='skills-title' onClick={handleSkillsClick}>Skills</div>
+                <i class="fa-solid fa-chevron-down"></i>
                 <div className={`logo-grid ${activeIndex === 2 ? 'active' : ''}`}>
                     {skillsLogos.map((items, index) => (
                             <div className='logo-item'>
