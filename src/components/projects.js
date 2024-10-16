511c2bf22d6568c6f3f0f17d495c57ba0f007204
Rob8624 <robert_melen@yahoo.co.uk>
2024-10-17 00:56:11 +0100

    added narrow images to side of projects section

--- src/components/projects.js
+++ src/components/projects.js
@@ -6,14 +6,15 @@ export default function Projects( { projectsData }) {
  return(
     
     <div className='projects-flex-wrapper'>
-       
-        <div className='projects-title'>Projects</div>
+       <div>-----</div>
+        <div className='main-projects-title'>Projects</div>
        
             { projectsData.map((items, index) => (
               <div className='projects-items-flex' key={items.id}>
                     <div className='description-flex'>
+                        <div className='projects-title'>{ items.title }</div>
                         <div className='projects-description'>{ items.description }</div>
-                        <div><strong>Stack: </strong>{ items.techstack } </div>
+                        <div className='tech-stack'><strong>Stack: </strong>{ items.techstack } </div>
                     </div>
                     <img className='projects-image' alt="productimage" src={ items.imageUrl }/>
                
