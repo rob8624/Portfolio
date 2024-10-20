869235010396d2a54ce335500903bc4ecbda7ac5
Rob8624 <robert_melen@yahoo.co.uk>
2024-10-20 14:13:39 +0100

    added gsap and slide animation to projects title

--- src/components/projects.js
+++ src/components/projects.js
@@ -1,13 +1,29 @@
 
+import gsap from 'gsap';
+
+import { useRef, useEffect } from 'react';
+
+
 import '../css/projects.css';
 
 export default function Projects( { projectsData }) {
     
- return(
+const titleRef = useRef(null);
+
+useEffect(() => {
+    const element = titleRef.current;
+    gsap.fromTo(element, 
+        { x: -100, opacity: 0 }, 
+        { x: 0, opacity: 1, duration: 2 })
+}, [])
+
+ 
+ 
+return(
     
     <div className='projects-flex-wrapper'>
        <div>-----</div>
-        <div className='main-projects-title'>Projects</div>
+        <div className='main-projects-title' ref={titleRef}>Projects</div>
        
             { projectsData.map((items, index) => (
               <div className='projects-items-flex' key={items.id}>
